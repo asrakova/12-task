3ebc873b039bedb76caae0b7d3a8d6dda1901626
Анна <@LAPTOP-63K8I5TL>
2017-08-31 22:00:15 +0500

    Реализации обеих сортировок

---
+++ Task 12/BinTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class BinTree
+    {
+        public int data;
+        public BinTree left,  //адрес левого поддерева 
+                       right; //адрес правого поддерева
+        public BinTree()
+        {
+            data = 0;
+            left = null;
+            right = null;
+        }
+        public BinTree(int d)
+        {
+            data = d;
+            left = null;
+            right = null;
+        }
+        public override string ToString()
+        {
+            return data + " ";
+        }
+
+        //формирование элемента дерева
+        public static BinTree MakeBinTree(int d)
+        {
+            BinTree p = new BinTree(d);
+            return p;
+        }
+
+        //добавление элемента d в дерево поиска
+        public static BinTree Add(BinTree tree, int d)
+        {
+            BinTree p = tree;//корень дерева
+            BinTree r = null;
+
+            bool ok = false;
+            if (tree.data == ' ') tree.data = d;
+            else
+            {
+                while (p != null && !ok)
+                {
+                    r = p;
+                    if (d < p.data) p = p.left;//пойти в левое поддерево
+                    else p = p.right; //пойти в правое поддерево
+                }
+                //создаем узел
+                BinTree NewTree = MakeBinTree(d);//выделили память
+
+                // если d<r->key, то добавляем его в левое поддерево
+                if (d < r.data) r.left = NewTree;
+                // если d>r->key, то добавляем его в правое поддерево
+                else r.right = NewTree;
+            }
+            return tree;
+        }
+        public static BinTree FindTree(int[] arr)
+        {
+            BinTree tree = new BinTree(arr[0]);
+            for (int i = 1; i < arr.Length; i++)
+            {
+                Add(tree, arr[i]);
+            }
+            return tree;
+        }
+
+        //печать дерева по уровням
+        public static void ShowTree(BinTree p, int l, ref int[] mas, ref int index)
+        {
+            if (p != null)
+            {
+                BinTree.ShowTree(p.left, l + 3, ref mas, ref index);//переход к левому поддереву
+                for (int i = 0; i < l; i++) Console.Write(" ");
+                Console.WriteLine(p.data);
+                mas[index] = p.data;
+                index++;
+                BinTree.ShowTree(p.right, l + 3, ref mas, ref index);//переход к правому поддереву
+            }
+        }
+
+    }
+}
--- Task 12/Program.cs
+++ Task 12/Program.cs
@@ -8,8 +8,44 @@ namespace Task_12
 {
     class Program
     {
+        static void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+            Console.WriteLine("Результат сортировки пузырьком: ");
+            for (int i = 0; i < 10; i++)
+                Console.Write("{0} ", arr[i]);
+            Console.WriteLine();
+        }
+
+        static void BinTreeSort(int[] arr)
+        {
+            BinTree k = BinTree.FindTree(arr);
+            Console.WriteLine("Результат сортировки с помощью бинарного дерева: ");
+            for (int i = 0; i < 10; i++)
+                Console.Write("{0} ", arr[i]);
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            Random a = new Random();
+            int[] mas = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                mas[i] = a.Next(10);
+                Console.Write("{0} ", mas[i]);
+            }
+            Console.WriteLine();
+            BubbleSort(mas);
+            BinTreeSort(mas);
+            Console.ReadLine();
         }
     }
 }
